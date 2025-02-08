7fb2f95043129c724a59213e41e841f32d8d55ff
ikwerre-dev <investorhonour@gmail.com>
2025-02-08 12:36:31 +0100

    updated

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -7,7 +7,6 @@ import { TopBar } from "@/components/dashboard/TopBar"
 import dynamic from "next/dynamic"
 const Chart = dynamic(() => import("react-apexcharts"), { ssr: false })
 import type { ApexOptions } from "apexcharts"
- 
 const timeFilters = ["1M", "5M", "15M", "30M", "1H"]
 
 const assets = [
